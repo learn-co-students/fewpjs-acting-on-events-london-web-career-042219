378687269b407ec7fe653d6b5a4a6190c31f18af
kwren96 <katherine.wren196@gmail.com>
2019-06-03 11:16:55 +0100

    Done.

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-    var rightNumbers = dodger.style.right.replace("px", "");
-    var right = parseInt(rightNumbers, 10);
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
    
-    if (right > 0) {
-      dodger.style.right = `${right - 1}px`;
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
     }
-  }
+}
 
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
